ca04c9d04a057afcdaf29783e38399ddb89cf13c
Tang U Liang <s42355@sp.edu.sg>
2018-03-02 13:11:43 +0800

    Scrape.py updated with CLI.

--- scrape.py
+++ scrape.py
@@ -6,14 +6,15 @@ Created on Wed Feb 28 21:46:53 2018
 @author: uliang
 """
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 from bs4 import BeautifulSoup, SoupStrainer
 from progress.spinner import Spinner
+from datetime import timedelta
 import numpy as np
 import pandas as pd
 import time
-
+import click
+import os
 
 #%% Helper functions
 def strip_text(x):
@@ -21,25 +22,34 @@ def strip_text(x):
                 for split_string
                 in ['', 'Available', 'Add to shortlist', 'Tags:']])
 
-def main(): 
+#%% Main application loop
+@click.command()
+@click.option('--headless/--no-headless', default=False, help='Whether not to show browsing'+
+              ' window or not')
+@click.option('--max_page', default=1, help='Max number of pages to scrape starting' +
+              ' from the most recent listings. Default is 1.'+
+              ' Pass -1 to scrape all listings.')
+def main(headless, max_page): 
+    MAX_COUNT = max_page
 
     options = webdriver.ChromeOptions()
-    options.add_argument("headless")
+    options.add_argument('log-level=3')
+    if headless: options.add_argument("headless")
+
     browser = webdriver.Chrome(chrome_options=options)
     browser.get('http://www.sgcarmart.com/used_cars/listing.php')
     sift = SoupStrainer('div', id='contentblank')
     spin = Spinner('Scraping...')
     
-    counter = 1
+    counter, start = 1, time.time() 
     while True:
         
-        start = time.time() 
         # Get listings
         html = BeautifulSoup(browser.page_source, "html.parser", parse_only=sift)
         listings = html.find_all('table', {'cellspacing':'0', 'cellpadding':'0',
                                            'width':'100%',
                                            'style': 'table-layout: fixed;'})
-    
+        spin.next()
         # Data processing
         _data = [listing.get_text().split('\n') for listing in listings]
         _data = [map(str.strip, _dat) for _dat in _data]   
@@ -51,19 +61,22 @@ def main():
         data_array = np.array([dat[:6]+[dat[-2]] for dat in data])
         frame = pd.DataFrame(data_array, columns=fields)
         frame.to_csv("./data/%d.csv" % counter)
-        
+        spin.next()
         ## Condition for clicking next
         no_link = html.find('span', class_='pageNoLink')
         next_link_number = no_link.next_sibling.next_sibling.text
     
-        if next_link_number.isalnum():
+        scrape_next_page = next_link_number.isalnum()
+        if MAX_COUNT != -1: 
+            scrape_next_page = scrape_next_page and counter < MAX_COUNT
             
-            #%% Search for next page button and click.
+        if scrape_next_page:
+            
+            # Search for next page button and click.
             go_to_next = browser.find_element_by_partial_link_text("Next")
             try:
                 go_to_next.click()
             except:
-                #print("pausing")
                 action = ActionChains(browser)
                 action.pause(20)
                 action.move_to_element(go_to_next)
@@ -73,12 +86,18 @@ def main():
             break
         
         counter += 1
-        print("Time taken: %.4f s" % (time.time()-start))
         spin.next()
         
     browser.quit()
+    time_taken = str(timedelta(seconds = time.time()-start))
+    print("Time taken: %s" % (time_taken))
     print("Done")
 #%%
 # TODO: Append to master list
 
-if __name__=="__main__": main()
+if __name__=="__main__": 
+    try:
+        os.system('cls')
+    except:
+        os.system('clear')
+    main()
